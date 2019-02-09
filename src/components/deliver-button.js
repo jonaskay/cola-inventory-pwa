f7dc8ff0a45b2e68678dedab7b063ab4c6d58354
Jonas Kay <jonaskay@iki.fi>
2019-02-09 10:38:38 +0200

    Add styling to deliver button

--- src/components/deliver-button.js
+++ src/components/deliver-button.js
@@ -1,11 +1,26 @@
-import { LitElement, html } from "lit-element";
+import { LitElement, html, css } from 'lit-element';
 
-import { SharedStyles } from "./shared-styles.js";
-import { ButtonSharedStyles } from "./button-shared-styles.js";
+import { SharedStyles } from './shared-styles.js';
+import { ButtonSharedStyles } from './button-shared-styles.js';
 
 class DeliverButton extends LitElement {
   static get styles() {
-    return [SharedStyles, ButtonSharedStyles];
+    return [
+      SharedStyles,
+      ButtonSharedStyles,
+      css`
+        button {
+          border: 2px solid var(--app-dark-text-color);
+          border-radius: 3px;
+          padding: 8px 16px;
+        }
+
+        button:hover {
+          border-color: var(--app-primary-color);
+          color: var(--app-primary-color);
+        }
+      `,
+    ];
   }
 
   render() {
@@ -15,4 +30,4 @@ class DeliverButton extends LitElement {
   }
 }
 
-customElements.define("deliver-button", DeliverButton);
+customElements.define('deliver-button', DeliverButton);
